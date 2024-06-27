e306cfdf203ff93e6d0e892ec5721decc62b70a8
정순민 <>
2024-06-27 15:26:12 +0900

    Initial commit

---
+++ src/test/java/com/jesomi/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesomi.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
